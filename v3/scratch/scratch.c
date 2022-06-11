4f48a3344e4139f9db1e8ea6b2ac38e20ce58d7e
Jonathan Parlett <parlettjon@gmail.com>
2022-06-11 14:30:58 -0400

    changes

--- v3/scratch/scratch.c
+++ v3/scratch/scratch.c
@@ -2,6 +2,7 @@
 // Program Name: scratch.c
 // Created: 2022-May-30
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 		
@@ -11,23 +12,6 @@
 #define ui unsigned int
 
 
-
-
-
 int main(){
-int A[3] = {4,5,6};
-printf("Val of base pointer A = 0x%x\n", A);
-printf("____________________________________\n");
-for(int i=0; i < 3; i++){
-printf("*(A+%d) = %d | ", i, *(A+i));
-printf("A[%d] = %d | ", i, A[i]);
-printf("Address of value at (A+%d) = 0x%x\n", i, (A+i));
-}
-// The size of an int is 4 bytes
-printf("Size of integer = %lu\n", sizeof(int));
-// The size of a char is a single byte
-printf("Size of char = %lu\n", sizeof(char));
-// The size of any pointer is 8 bytes, no matter the type it points to.
-printf("Size of int* = %lu\n", sizeof(int*));
-printf("Size of char* = %lu\n", sizeof(char*));
+
 }
